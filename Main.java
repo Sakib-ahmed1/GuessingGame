a8caf594e2328b6acc7ce22499900e8f42e2c3cb
Sakib Ahmed <153700021+Sakib-ahmed1@users.noreply.github.com>
2024-03-03 08:51:16 -0500

    Main.java

--- Main.java
+++ Main.java
@@ -3,23 +3,22 @@ package com.coderscampus.GuessingGame;
 import java.util.Scanner;
 
 public class Main {
+	
+    public static void main(String[] args) {
+    	
+        GuessingGame game = new GuessingGame();
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
+        while (!game.isGameOver()) {
+            System.out.println("Pick a number between 1 and 100");
+            int guess = scanner.nextInt();
+            System.out.println(game.guess(guess));
+        }
 
-		GuessingGame game = new GuessingGame();
-		Scanner scanner = new Scanner(System.in);
+        if (!game.hasWon()) {
+            System.out.println("You lose, the number to guess was " + game.getTheNumber());
+        }
 
-		while (!game.isGameOver()) {
-			System.out.println("Pick a number between 1 and 100");
-			int guess = scanner.nextInt();
-			System.out.println(game.guess(guess));
-		}
-
-		if (!game.hasWon()) {
-			System.out.println("You lose, the number to guess was " + game.getTheNumber());
-		}
-
-		scanner.close();
-	}
-
+        scanner.close();
+    }
 }
